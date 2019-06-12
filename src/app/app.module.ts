5610bab45ffbe86ed3e26677bc5e7cd716e9ed0b
Cristian <criswxart@gmail.com>
2019-06-12 02:55:33 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgessComponent } from './pages/progess/progess.component';
+import { GraficosComponent } from './pages/graficos/graficos.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+//RUtas
+import { APP_ROUTES } from './app.routes';
+import { CreateComponent } from './pages/profesionales/create.component';
+import { PagesComponent } from './pages/pages.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { ProfesionalesComponent } from './pages/profesionales/profesionales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgessComponent,
+    GraficosComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    PagesComponent,
+    UsuariosComponent,
+    ProfesionalesComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/graficos/graficos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficos',
+  templateUrl: './graficos.component.html',
+  styles: []
+})
+export class GraficosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
